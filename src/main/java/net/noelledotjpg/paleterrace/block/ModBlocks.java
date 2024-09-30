f4626d7c8bd449fb0375c04a752d533e2d9a431c
noelledotjpg <ruthra2016.1@gmail.com>
2024-09-30 13:24:33 -0300

    test

--- src/main/java/net/noelledotjpg/paleterrace/block/ModBlocks.java
+++ src/main/java/net/noelledotjpg/paleterrace/block/ModBlocks.java
@@ -2,7 +2,9 @@ package net.noelledotjpg.paleterrace.block;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,7 +17,16 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> PALE_DIRT = BLOCKS.register("pale_dirt",
             () -> new Block(new BlockBehaviour.Properties()
-                    .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+                    .of(Material.DIRT).strength(0.5F)
+                    .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
+                    .sound(SoundType.MUD)));
+
+    public static final RegistryObject<Block> WILTED_GRASS_BLOCK = BLOCKS.register("wilted_grass_block",
+            () -> new Block(new BlockBehaviour.Properties().destroyTime()
+                    .of(Material.DIRT).strength(0.5F)
+                    .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
+                    .sound(SoundType.MUD)
+                    .sound(SoundType.GRASS)));
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
